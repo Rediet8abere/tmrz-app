a3580b34f3d9c910f59a9c2035ca36b8eb51ad20
Rediet8abere <redietaberegesesse@gmail.com>
2020-09-30 17:00:34 -0700

    Updates

---
+++ precache-manifest.516cfbed33f3fd75259f0a4fe28baa41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d0dd864e9499a02d1c0a5fd7777025c",
+    "url": "/tmrz-app/index.html"
+  },
+  {
+    "revision": "6d96c46c1fcff4830256",
+    "url": "/tmrz-app/static/css/main.da5f3aba.chunk.css"
+  },
+  {
+    "revision": "aa599ae97355bb509c42",
+    "url": "/tmrz-app/static/js/2.1593ede2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/tmrz-app/static/js/2.1593ede2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d96c46c1fcff4830256",
+    "url": "/tmrz-app/static/js/main.ac5ab2c6.chunk.js"
+  },
+  {
+    "revision": "4fe742ada06a3f747964",
+    "url": "/tmrz-app/static/js/runtime-main.c367a34e.js"
+  }
+]);
